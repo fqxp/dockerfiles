592a6293538fa483ba68807f46b1b1b9fbfdd11b
Frank Ploss <frank@fqxp.de>
2014-01-26 17:31:57 +0100

    add new base Dockerfile

--- apache2/start.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-mkdir /var/run/apache2
-. /etc/apache2/envvars
-
-/usr/sbin/apache2 -D FOREGROUND -X
---
+++ base/start.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+if [ -n "$AUTHORIZED_KEYS" ]; then
+  echo "$AUTHORIZED_KEYS" >> /root/.ssh/authorized_keys
+fi
+
+/usr/bin/supervisord --nodaemon
