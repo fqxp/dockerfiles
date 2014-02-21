7fc59face0cb7a6220992293dacad66a154157cd
Frank Ploss <frank@fqxp.de>
2014-02-21 11:58:12 +0100

    add base README and improve base image

--- base/start.sh
+++ base/start.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 if [ -n "$AUTHORIZED_KEYS" ]; then
-  echo "$AUTHORIZED_KEYS" >> /root/.ssh/authorized_keys
+  echo "$AUTHORIZED_KEYS" > /root/.ssh/authorized_keys
 fi
 
 /usr/bin/supervisord --nodaemon
