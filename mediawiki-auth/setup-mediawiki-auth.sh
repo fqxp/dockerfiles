7e9b44c146c5fae3f14a97ac8395aebf98c8e59f
Frank Ploss <frank@fqxp.de>
2014-05-29 01:27:37 +0200

    Pass REMOTE_USER to fastcgi process and setup mediawiki correctly

--- mediawiki-auth/setup-mediawiki-auth.sh
+++ mediawiki-auth/setup-mediawiki-auth.sh
@@ -3,7 +3,7 @@
 if ! grep -q Auth_remoteuser /srv/mediawiki/LocalSettings.php ; then
   cat >>/srv/mediawiki/LocalSettings.php <<EOF
 
-require_once("$IP/extensions/Auth_remoteuser/Auth_remoteuser.php");
-$wgAuth = new Auth_remoteuser();
+require_once("\$IP/extensions/Auth_remoteuser/Auth_remoteuser.php");
+\$wgAuth = new Auth_remoteuser();
 EOF
 fi
